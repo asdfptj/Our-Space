fa691225935ef139aab8916e0d2a06fb9a7f605e
asdftaejun <taejunb70@gmail.com>
2023-11-21 16:58:52 +0900

    [black] Responsive ing

--- dist/asset/resource/js/main.js
+++ dist/asset/resource/js/main.js
@@ -1,11 +1,19 @@
-const content = "Mysterious universe, endless exploration"
-const text = document.querySelector(".main-title")
-let index = 0;
-
-function typing(){
-    text.textContent += content[index++]
-    if(index > content.length){
-      text.textContent = "Mysterious universe, endless exploration"
+$(function(){
+  const content = "Mysterious universe, endless exploration"
+  const text = document.querySelector(".main-title")
+  let index = 0;
+  
+  function typing(){
+      text.textContent += content[index++]
+      if(index > content.length){
+        text.textContent = "Mysterious universe, endless exploration"
+      }
     }
-  }
   setInterval(typing, 70)
+})
+
+$(document).ready(function() {
+  $('.burger-menu').click(function() {
+      $('.header-nav').toggleClass('show');
+  });
+});
